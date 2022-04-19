1c57978e98512ee8e3d444c257964af0ade78b38
emir.vranac <vranacemir@gmail.com>
2022-04-19 23:04:36 +0200

    style login form

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import useSecurity from "./useSecurity"
+import "./assets/styles/login.css"
 
 const Login = () => {
   const [username, setUsername] = useState("")
@@ -8,31 +9,42 @@ const Login = () => {
   const { login } = useSecurity()
 
   return (
-    <>
-      <h1>Login</h1>
+    <div className='wrapper'>
+      <div className='form-wrapper'>
+        <h1 className='text-center'>Login</h1>
+        <form>
+          <div className='form-control'>
+            <label htmlFor='username'>Username:</label>
+            <input
+              id='username'
+              name='username'
+              className='form-field'
+              type='text'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
 
-      <label htmlFor='username'>Username:</label>
-      <input
-        id='username'
-        name='username'
-        type='text'
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
+          <div className='form-control'>
+            <label htmlFor='password'>Password:</label>
+            <input
+              id='password'
+              className='form-field'
+              name='password'
+              type='text'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
 
-      <label htmlFor='password'>Password:</label>
-      <input
-        id='password'
-        name='password'
-        type='text'
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-
-      <br />
-
-      <button onClick={() => login(username, password)}>Login</button>
-    </>
+          <div className='form-control text-center'>
+            <button className='btn' onClick={() => login(username, password)}>
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
